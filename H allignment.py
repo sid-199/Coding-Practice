5d75ceffd6f9076ea3b44a0baa8c2c0f2c0dd96c
sid-199 <siddheshyadav19@gmail.com>
2026-02-10 21:51:25 +0530

    HackerRank question

---
+++ H allignment.py
@@ -0,0 +1,17 @@
+w=int(input())
+
+for i in range(w):
+    print(('H'*i).rjust(w-1) + 'H' + ('H'*i).ljust(w-1))
+
+for i in range(w+1):
+    print(('H'*w).center(w*2) + ('H'*w).center(w*6))
+
+for i in range((w+1)//2):
+    print(('H'*w*5).center(w*6))
+
+for i in range(w+1):
+    print(('H'*w).center(w*2) + ('H'*w).center(w*6))
+
+
+for i in range(w):
+    print((('H'*(w-i-1)).rjust(w) + 'H' + ('H'*(w-i-1)).ljust(w)).rjust(w*6))
