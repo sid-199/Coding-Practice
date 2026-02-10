8177c83e84e860666017dd43922f49c9f8a7aac7
sid-199 <siddheshyadav19@gmail.com>
2026-02-10 21:52:02 +0530

    Update H allignment.py

--- H allignment.py
+++ H allignment.py
@@ -1,4 +1,4 @@
-w=int(input())
+w=int(input("Enter odd no.:"))
 
 for i in range(w):
     print(('H'*i).rjust(w-1) + 'H' + ('H'*i).ljust(w-1))
@@ -15,3 +15,4 @@ for i in range(w+1):
 
 for i in range(w):
     print((('H'*(w-i-1)).rjust(w) + 'H' + ('H'*(w-i-1)).ljust(w)).rjust(w*6))
+
