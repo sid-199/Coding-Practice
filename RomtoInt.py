5bd8a67446c1746ec7c8d724b1806efb25a6fe33
sid-199 <siddheshyadav19@gmail.com>
2026-03-24 11:35:56 +0530

    Create RomtoInt.py
    practice from leet code questions.

---
+++ RomtoInt.py
@@ -0,0 +1,18 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        rom={
+            "I":1,"V":5,"X":10,
+            "L":50,"C":100,"D":500,
+            "M":1000
+        }
+
+        total=0
+
+        for i in range (len(s)):
+            if i+1<len(s) and rom[s[i]] < rom[s[i+1]]:
+                total-=rom[s[i]]
+
+            else:
+                total+=rom[s[i]]
+
+        return total
